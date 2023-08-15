2835a5b121e0a1fb5edbc15f52f6d05d12df9288
Filipi Brabo <filipi.brabo@gmail.com>
2023-08-14 23:34:55 -0300

    🎉 Create parse ranking back end app

---
+++ apps/parse-ranking-back-end/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'parse-ranking-back-end',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/parse-ranking-back-end',
+};
