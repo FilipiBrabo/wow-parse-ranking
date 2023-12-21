78d8e9cf881b11cb5c931f030a9c274510f4ba8c
Filipi Brabo <filipi.brabo@gmail.com>
2023-12-21 15:24:59 -0300

    🎉 Add parse ranking front end app

---
+++ apps/parse-ranking-front-end/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'parse-ranking-front-end',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/parse-ranking-front-end',
+};
