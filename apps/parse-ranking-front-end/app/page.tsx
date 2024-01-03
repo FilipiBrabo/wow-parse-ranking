e4324527ae1c2d94629654be40594ac1257178d7
Filipi Brabo <filipi.brabo@gmail.com>
2024-01-03 13:56:09 -0300

    🚧 Remove api call

--- apps/parse-ranking-front-end/app/page.tsx
+++ apps/parse-ranking-front-end/app/page.tsx
@@ -1,26 +1,22 @@
-import { Top25CharactersTable } from '../src/components/Top25CharactersTable';
+// async function getTop25Characters() {
+//   const response = await fetch(
+//     'http://localhost:3000/api/rankings/top25?encounterIds=5,6,7,8,9,10,11,12,13'
+//   );
 
-async function getTop25Characters() {
-  const response = await fetch(
-    'http://localhost:3000/api/rankings/top25?encounterIds=5,6,7,8,9,10,11,12,13'
-  );
+//   if (!response.ok) {
+//     throw new Error('Failed to fetch top 25 players');
+//   }
 
-  if (!response.ok) {
-    throw new Error('Failed to fetch top 25 players');
-  }
-
-  return response.json();
-}
+//   return response.json();
+// }
 
 export default async function Index() {
-  const topCharacters = await getTop25Characters();
+  // const topCharacters = await getTop25Characters();
 
   return (
     <div className="flex flex-col justify-center items-center min-h-screen w-20000 bg-gray-100 text-gray-900">
       <h1 className="text-4xl font-bold underline mb-4">Top Characters</h1>
-      <div>
-        <Top25CharactersTable data={topCharacters} />
-      </div>
+      <div>{/* <Top25CharactersTable data={topCharacters} /> */}</div>
     </div>
   );
 }
