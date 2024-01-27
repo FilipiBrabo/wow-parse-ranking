6b08eb265c68edbb1c4c763aad2cadebc0c116ea
Filipi Brabo <filipi.brabo@gmail.com>
2024-01-26 22:53:38 -0300

    🐛 Fix ranks path endpoint

--- apps/parse-ranking-front-end/app/ranks/page.tsx
+++ apps/parse-ranking-front-end/app/ranks/page.tsx
@@ -2,9 +2,7 @@ import { RanksTable } from './components/ranks-table';
 import { Character, columns } from './components/table-columns';
 
 async function getRanks(): Promise<Character[]> {
-  const response = await fetch(
-    `${process.env.API_BASE_PATH}/rankings/top25?encounterIds=5,6,7,8,9,10,11,12,13`
-  );
+  const response = await fetch(`${process.env.API_BASE_PATH}/rankings/icc`);
 
   if (!response.ok) {
     throw new Error('Failed to fetch top 25 players');
