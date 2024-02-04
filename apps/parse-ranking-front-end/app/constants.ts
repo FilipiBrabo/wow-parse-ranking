702f434104669ffe739db1f7ce8d7fa2cbffd123
Filipi Brabo <filipi.brabo@gmail.com>
2024-02-04 19:14:40 -0300

    🐛 Fix class images import path

--- apps/parse-ranking-front-end/app/constants.ts
+++ apps/parse-ranking-front-end/app/constants.ts
@@ -1,13 +1,13 @@
-import DeathKnightIcon from '../public/images/class-deathknight.jpg';
-import DruidIcon from '../public/images/class-druid.jpg';
-import HunterIcon from '../public/images/class-hunter.jpg';
-import MageIcon from '../public/images/class-mage.jpg';
-import PaladinIcon from '../public/images/class-paladin.jpg';
-import PriestIcon from '../public/images/class-priest.jpg';
-import RogueIcon from '../public/images/class-rogue.jpg';
-import ShamanIcon from '../public/images/class-shaman.jpg';
-import WarlockIcon from '../public/images/class-warlock.jpg';
-import WarriorIcon from '../public/images/class-warrior.jpg';
+import DeathKnightIcon from '../public/images/classes/class-deathknight.jpg';
+import DruidIcon from '../public/images/classes/class-druid.jpg';
+import HunterIcon from '../public/images/classes/class-hunter.jpg';
+import MageIcon from '../public/images/classes/class-mage.jpg';
+import PaladinIcon from '../public/images/classes/class-paladin.jpg';
+import PriestIcon from '../public/images/classes/class-priest.jpg';
+import RogueIcon from '../public/images/classes/class-rogue.jpg';
+import ShamanIcon from '../public/images/classes/class-shaman.jpg';
+import WarlockIcon from '../public/images/classes/class-warlock.jpg';
+import WarriorIcon from '../public/images/classes/class-warrior.jpg';
 
 export const ICC_RANKINGS_TAG = 'icc-rankings-tag';
 
