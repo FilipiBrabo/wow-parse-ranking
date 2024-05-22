9c593bc0fc02bff6355e92104550004aedaa05b8
Filipi Brabo <filipi.brabo@gmail.com>
2024-05-21 21:30:43 -0300

    🔥 Remove route invalidation endpoint

--- apps/parse-ranking-front-end/app/api/invalidate-ranks/route.ts
+++
@@ -1,8 +0,0 @@
-import { revalidateTag } from 'next/cache';
-
-import { ICC_RANKINGS_TAG } from '../../constants';
-
-export async function GET() {
-  revalidateTag(ICC_RANKINGS_TAG);
-  return new Response();
-}
