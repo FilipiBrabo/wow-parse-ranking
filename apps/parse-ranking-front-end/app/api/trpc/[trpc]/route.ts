3f74af50eae4a19f502bf44d359ea4c19705d398
Filipi Brabo <filipi.brabo@gmail.com>
2024-06-09 18:11:39 -0300

    🐛 Increase trpc timeout

--- apps/parse-ranking-front-end/app/api/trpc/[trpc]/route.ts
+++ apps/parse-ranking-front-end/app/api/trpc/[trpc]/route.ts
@@ -2,6 +2,9 @@ import { fetchRequestHandler } from '@trpc/server/adapters/fetch';
 
 import { appRouter } from '../../../../src/server';
 
+export const maxDuration = 60;
+export const dynamic = 'force-dynamic';
+
 const handler = (req: Request) =>
   fetchRequestHandler({
     endpoint: '/api/trpc',
