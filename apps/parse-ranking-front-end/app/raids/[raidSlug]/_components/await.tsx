8a41708010edf21152484f3edee9e6be3c968699
Filipi Brabo <filipi.brabo@gmail.com>
2024-12-27 22:01:14 -0300

    ⬆️ Update nx to v20

--- apps/parse-ranking-front-end/app/raids/[raidSlug]/_components/await.tsx
+++ apps/parse-ranking-front-end/app/raids/[raidSlug]/_components/await.tsx
@@ -1,9 +1,11 @@
+import * as React from 'react';
+
 export async function Await<T>({
   promise,
   children,
 }: {
   promise: Promise<T>;
-  children: (data: T) => JSX.Element;
+  children: (data: T) => React.ReactNode;
 }) {
   const data = await promise;
 
